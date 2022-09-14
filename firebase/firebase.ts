06a87834bcb2e1f459cd43c2f3b488ab49b4e5aa
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-09-14 11:56:08 +0200

    added firebase config

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,8 +1,17 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    //...
+    apiKey: "AIzaSyAnuIvpQOPYNp5wN6GQkf1-z9ntJyF85e4",
+    authDomain: "pomodoro-7c5c8.firebaseapp.com",
+    projectId: "pomodoro-7c5c8",
+    storageBucket: "pomodoro-7c5c8.appspot.com",
+    messagingSenderId: "670837134867",
+    appId: "1:670837134867:web:1244005c3a31f714dcd1e6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
