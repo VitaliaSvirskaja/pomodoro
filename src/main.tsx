6dce919db2229515396d665a9c96140d82203517
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-09-18 14:01:27 +0200

    added authContext  and settingsContext provider on top level

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SettingsContextProvider } from "./context/SettingsContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <SettingsContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SettingsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
