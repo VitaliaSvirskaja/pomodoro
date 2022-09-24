13e2ac9d85f8e53cd7b962b2f8f209bc54073f65
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-09-24 08:30:28 +0200

    refactored settings to open a dialog with overlay

--- src/components/TimerSettings.tsx
+++ src/components/TimerSettings.tsx
@@ -1,85 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useSettingsContext } from "../context/SettingsContext";
+import React, { useState } from "react";
 import settingsIcon from "../assets/settingsIcon.png";
 import styles from "./TimerSettings.module.css";
+import { SettingsDialog } from "./SettingsDialog";
 
 export const TimerSettings = () => {
-  const { saveSettings, defaultTimer } = useSettingsContext();
-  const [pomodoroTimer, setPomodoroTimer] = useState(defaultTimer.pomodoro);
-  const [shortBreakTimer, setShortBreakTimer] = useState(
-    defaultTimer.shortBreak
-  );
-  const [longBreakTimer, setLongBreakTimer] = useState(defaultTimer.longBreak);
-  const [isSettingVisible, setIsSettingVisible] = useState(false);
-
-  useEffect(() => {
-    setPomodoroTimer(defaultTimer.pomodoro);
-    setShortBreakTimer(defaultTimer.shortBreak);
-    setLongBreakTimer(defaultTimer.longBreak);
-  }, [defaultTimer]);
-
-  function changePomodoro(input: React.ChangeEvent<HTMLInputElement>) {
-    const newPomodoro = parseInt(input.target.value);
-    setPomodoroTimer(newPomodoro);
-  }
-
-  function changeShortBreak(input: React.ChangeEvent<HTMLInputElement>) {
-    const newShortBreak = parseInt(input.target.value);
-    setShortBreakTimer(newShortBreak);
-  }
-
-  function changeLongBreak(input: React.ChangeEvent<HTMLInputElement>) {
-    const newLongBreak = parseInt(input.target.value);
-    setLongBreakTimer(newLongBreak);
-  }
-
-  function handleSaveSettings(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    saveSettings(pomodoroTimer, shortBreakTimer, longBreakTimer);
-    setIsSettingVisible(false);
-  }
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   function handleSettingsClick() {
-    setIsSettingVisible(!isSettingVisible);
+    console.log("Settings clicked");
+    setIsDialogOpen(!isDialogOpen);
   }
+
+  const confirm = () => {
+    setIsDialogOpen(false);
+  };
 
   return (
     <div>
-      <img
-        src={settingsIcon}
-        alt="settings-icon"
-        className={styles.settingsIcon}
-        onClick={handleSettingsClick}
-      />
-
-      <form
-        onSubmit={handleSaveSettings}
-        className={styles.settingsform}
-        style={{ visibility: isSettingVisible ? "visible" : "hidden" }}
-      >
-        <label htmlFor="pomodoro">Pomodoro:</label>
-        <input
-          type="number"
-          name="pomodoro"
-          onChange={changePomodoro}
-          value={pomodoroTimer}
+      <div>
+        <img
+          src={settingsIcon}
+          alt="settings-icon"
+          className={styles.settingsIcon}
+          onClick={handleSettingsClick}
         />
-        <label htmlFor="short break">Short Break:</label>
-        <input
-          type="number"
-          name="short break"
-          onChange={changeShortBreak}
-          value={shortBreakTimer}
-        />
-        <label htmlFor="long break">Long Break:</label>
-        <input
-          type="number"
-          name="long break"
-          onChange={changeLongBreak}
-          value={longBreakTimer}
-        />
-        <button type="submit">Save</button>
-      </form>
+      </div>
+      <SettingsDialog isOpen={isDialogOpen} confirm={confirm} />
     </div>
   );
 };
