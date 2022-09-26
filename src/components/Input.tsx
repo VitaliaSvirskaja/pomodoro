02c5cf9ddf6bb1b8d250e311b8d2e61b2d7d0ae3
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-09-26 18:30:06 +0200

    Refactored styling of input component to include space for the error message

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,16 +2,25 @@ import React, { InputHTMLAttributes } from "react";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  error: string;
 }
-export function Input({ label, ...inputProps }: Props) {
+export function Input({ error, label, ...inputProps }: Props) {
   return (
-    <div className="flex items-center gap-4 justify-between">
-      <label htmlFor={label}>{label}:</label>
-      <input
-        {...inputProps}
-        name={label}
-        className="w-20 rounded border-2 p-1"
-      />
+    <div className="flex flex-col items-center gap-0 justify-between h-28">
+      <div className="flex flex-col items-center gap-4 justify-between">
+        <label htmlFor={label}>{label}:</label>
+        <input
+          {...inputProps}
+          name={label}
+          className={
+            "w-24 rounded border-2 p-1 " +
+            (error !== "" ? "border-red-600" : "")
+          }
+        />
+      </div>
+      <div>
+        <span className="text-xs text-red-600">{error}</span>
+      </div>
     </div>
   );
 }
