6f76333811841b5ba394578a98888349b83dd3ff
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-09-28 20:15:08 +0200

    customized theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors:{
+      primary:{
+        DEFAULT:'#7E88CA',
+        dark:'#6672B9',
+        light: '#EDEEF5'
+      },
+    },
     extend: {
       backgroundImage:{
         "home-background": "url('src/assets/pomodoro-background.jpg')"
