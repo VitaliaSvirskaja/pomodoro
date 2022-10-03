325873227d4b0f76ae35bf92b4e49a7db97cc4d7
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-03 10:22:30 +0200

    fix falsy error indicator

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -22,13 +22,9 @@ export function Input({ error, label, variant, ...inputProps }: Props) {
       <input
         {...inputProps}
         name={label}
-        className={
-          "rounded border border-2 py-2 px-3 outline-0 focus:ring focus:ring-2" +
-          " " +
-          (error !== "" ? "focus:ring-red-500" : "focus:ring-violet-500") +
-          " " +
-          (variant === "filled" ? "bg-gray-200" : "")
-        }
+        className={`rounded border border-2 py-2 px-3 outline-0 focus:ring focus:ring-2 ${
+          error === undefined ? "focus:ring-violet-500" : "focus:ring-red-500"
+        } ${variant === "filled" ? "bg-gray-100" : ""}`}
       />
 
       <span className="pl-1 text-xs text-red-600">{error}</span>
