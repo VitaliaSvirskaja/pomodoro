789d13b70ce9fb641ab2b6e38f6855e6805a501b
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-10-05 21:34:09 +0200

    added delete account route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { ResetPassword } from "./pages/ResetPassword";
+import { DeleteAccount } from "./pages/DeleteAccount";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/delete-account" element={<DeleteAccount />} />
       </Routes>
     </>
   );
